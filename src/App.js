bd9a5ab4330b2c66e3deaf318ba6af54eb6106c1
elikese <elikese@gmail.com>
2024-02-20 02:47:32 +0900

    feat : count button 동작

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 export default function App() {
+  
+  const [count, setCount] = useState(0);
+  function handlePlusClick() {
+    setCount(num => num + 1);
+  }
+  function handleMinusClick() {
+    setCount(num => num - 1);
+  }
+
   return (
     <>
-
+     <div>
+      <h1>{count}</h1>
+      <button onClick={handlePlusClick}>+1</button>
+      <button onClick={handleMinusClick}>-1</button>
+     </div>
     </>
   );
 }
